6e43afcf242262867abff567d8a67031c3ee9759
Omkar Asgaonkar <omasgaonkar@gmail.com>
2024-02-19 23:06:11 +0530

    Add project files.

---
+++ NoPowershell/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace NoPowershell
+{
+    internal class Pawshell
+    {
+        
+
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.Write("Maverick PS> ");
+                var command = Console.ReadLine();
+                if (command.Equals("exit"))
+                {
+                    return;
+                }
+                else{
+                    try
+                    {
+                        // Console.WriteLine(command); // Just for Debugging
+                        using (var powershell = PowerShell.Create())
+                        {
+                            powershell.AddScript(command);
+                            var results = powershell.Invoke();
+                            foreach (var result in results){
+                                Console.WriteLine(result);
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Some Error occured");        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NoPowershellUninstallVariant/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace NoPowershellUninstallVariant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the main method which is a decoy");
+        }
+
+        [System.ComponentModel.RunInstaller(true)]
+        public class Sample : System.Configuration.Install.Installer
+        {
+            public override void Uninstall(System.Collections.IDictionary savedState)
+            {
+                Console.WriteLine("This is uninstall");
+                while (true)
+                {
+                    Console.Write("Maverick PS> ");
+                    var command = Console.ReadLine();
+                    if (command.Equals("exit"))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            // Console.WriteLine(command); // Just for Debugging
+                            using (var powershell = PowerShell.Create())
+                            {
+                                powershell.AddScript(command);
+                                var results = powershell.Invoke();
+                                foreach (var result in results)
+                                {
+                                    Console.WriteLine(result);
+                                }
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Some Error occured");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
